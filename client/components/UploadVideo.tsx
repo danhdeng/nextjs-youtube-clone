fbeafc33996f1618c6369f10fe94edd97a683ccd
danhdeng <hui_danny_deng@hotmail.com>
2022-04-23 21:25:37 -0400

    update uploadVideo.tsx

--- client/components/UploadVideo.tsx
+++ client/components/UploadVideo.tsx
@@ -1,4 +1,13 @@
-import { Button, Group, Modal, Stack, Switch, TextInput,Text, Progress } from '@mantine/core';
+import {
+  Button,
+  Group,
+  Modal,
+  Stack,
+  Switch,
+  TextInput,
+  Text,
+  Progress,
+} from '@mantine/core';
 import { Dropzone, MIME_TYPES } from '@mantine/dropzone';
 import { useForm } from '@mantine/hooks';
 import { AxiosError, AxiosResponse } from 'axios';
@@ -57,47 +66,47 @@ function EditVideoForm({
         <Switch label="Published" {...form.getInputProps('published')} />
         <Button type="submit">Save</Button>
       </Stack>
-    </form
+    </form>
+  );
 }
 
 export default function UploadVideo() {
-  const [opened, setOpened]=useState(false);
-  const [progress, setProgress]=useState(0);
+  const [opened, setOpened] = useState(false);
+  const [progress, setProgress] = useState(0);
   const mutation = useMutation(uploadVideo);
-  const config ={
-    onUploadProgress:(processEvent: any)=>{
-      const percent=Math.round(
-        (processEvent.loaded *100)/ processEvent.total
+  const config = {
+    onUploadProgress: (processEvent: any) => {
+      const percent = Math.round(
+        (processEvent.loaded * 100) / processEvent.total
       );
       setProgress(percent);
     },
   };
 
-  function upload(files:File[]){
-    const formData=new FormData();
-    formData.append("video", files[0]);
-    mutation.mutate({formData, config});
+  function upload(files: File[]) {
+    const formData = new FormData();
+    formData.append('video', files[0]);
+    mutation.mutate({ formData, config });
   }
   return (
     <>
       <Modal
         closeOnClickOutside={false}
-        onClose={()=>setOpened(false)}
+        onClose={() => setOpened(false)}
         opened={opened}
         title="Upload Video"
         size="xl"
       >
-        {progress ===0 && (
+        {progress === 0 && (
           <Dropzone
-            onDrop={(files)=>{
-              upload(files)
+            onDrop={(files) => {
+              upload(files);
             }}
             accept={[MIME_TYPES.mp4]}
             multiple={false}
-
           >
-            {(status)=>{
-              return(
+            {(status) => {
+              return (
                 <Group>
                   <ArrowBigUpLine />
                   <Text>Drag video here or click to find</Text>
@@ -106,18 +115,18 @@ export default function UploadVideo() {
             }}
           </Dropzone>
         )}
-        {progress >0 && (
+        {progress > 0 && (
           <Progress size="xl" label={`${progress}%`} value={progress} mb="xl" />
         )}
 
         {mutation.data && (
-          <EditVideoForm 
-            setOpened={setOpened} 
-            videoId={mutation.data.videoId} 
+          <EditVideoForm
+            setOpened={setOpened}
+            videoId={mutation.data.videoId}
           />
         )}
       </Modal>
-      <Button onClick={()=>setOpened(true)}>Upload video</Button>
+      <Button onClick={() => setOpened(true)}>Upload video</Button>
     </>
   );
 }
